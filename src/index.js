bbe9cd6f5b50646009a919bd77506721a283ae03
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-11-01 16:10:00 +0100

    add switch operator

--- src/index.js
+++ src/index.js
@@ -4,92 +4,141 @@ import { multiply } from './operations/multiply.js';
 import { subtract } from './operations/subtract.js';
 
 let display = document.getElementById('currentValue');
-
-
+let smallDisplay = document.getElementById('lastOperation');
 
 let currentValue = '0';
-let previousValue = null;
+let firstValue = null;
+let secondValue = null;
 let operator = null;
-
+let lastOperation = null;
 
 function updateDisplay() {
   display.value = currentValue;
+  smallDisplay.value = lastOperation;
   display.scrollLeft = display.scrollWidth;
+  console.log('firstValue: ', firstValue);
+  console.log('operator: ', operator);
+  console.log('secondValue: ', secondValue);
+  console.log('-------------');
 }
 
-function handleNumber(number) {
-  currentValue === '0' ? (currentValue = number) : (currentValue += number);
+function handleNumber(numberOrDot) {
+  if (numberOrDot === '.') {
+    if (!operator && !currentValue.toString().includes('.')) {
+      currentValue += '.';
+    } else if (
+      operator &&
+      secondValue &&
+      !secondValue.toString().includes('.')
+    ) {
+      currentValue += '.';
+    }
+  } else {
+    if (secondValue === '0' ) {
+      currentValue = currentValue.toString().slice(0, -1);
+      currentValue += numberOrDot;
+    } else {
+      currentValue == 0 || currentValue == 'Divide by zero'
+        ? (currentValue = numberOrDot)
+        : (currentValue += numberOrDot);
+    }
+  }
+
+  if (operator) {
+    secondValue = currentValue
+      .toString()
+      .split(firstValue + operator)
+      .join('');
+  }
   updateDisplay();
 }
 
 function handleOperator(currentOperator) {
-  if (operator && previousValue && currentValue) {
-    calculate(previousValue, currentValue, operator);
-    previousValue = null;
+  if (operator && secondValue) {
+    calculate();
+  } else if (operator && !secondValue) {
+    operator = currentOperator;
+    currentValue = currentValue.toString().slice(0, -1);
   }
-
   operator = currentOperator;
-  previousValue = currentValue;
-  currentValue += currentOperator;
+  firstValue = currentValue;
+  if (currentValue !== 'Divide by zero') {
+    currentValue += operator;
+  }
 
   updateDisplay();
 }
 
-function calculate(a, b, operation) {
-  b = currentValue.split(previousValue + operator).join('');
-  const value1 = parseFloat(a);
-  const value2 = parseFloat(b);
-  if (operation === '+') currentValue = add(value1, value2);
-  else if (operation === '-') currentValue = subtract(value1, value2);
-  else if (operation === '*') currentValue = multiply(value1, value2);
-  else if (operation === '/') currentValue = divide(value1, value2);
-  if (currentValue.toString().includes('.')) {
-    if (currentValue.toString().split('.')[1].length > 5) {
-      currentValue = parseFloat(Number(currentValue).toFixed(5));
+function calculate(negate = 0) {
+  firstValue = parseFloat(currentValue);
+  secondValue = parseFloat(secondValue);
+  console.log('firstValue:', firstValue);
+  console.log('operator:', operator);
+  console.log('secondValue:', secondValue);
+  console.log('-------------');
+  lastOperation = currentValue;
+  if (negate) {
+    lastOperation = `-(${currentValue})`;
+  }
+
+  if (operator === '+') currentValue = add(firstValue, secondValue);
+  else if (operator === '-') currentValue = subtract(firstValue, secondValue);
+  else if (operator === '*') currentValue = multiply(firstValue, secondValue);
+  else if (operator === '/') {
+    currentValue = divide(firstValue, secondValue);
+    if (secondValue == 0) {
+      currentValue = 'Divide by zero';
     }
   }
 
-  updateDisplay();
-  previousValue = null;
+  if (currentValue.toString().includes('.')) {
+    if (currentValue.toString().split('.')[1].length > 9) {
+      currentValue = parseFloat(Number(currentValue).toFixed(9));
+    }
+  }
+  firstValue = currentValue;
+  secondValue = null;
   operator = null;
+  updateDisplay();
 }
 
 document.querySelectorAll('.btn').forEach((button) => {
   button.addEventListener('click', (event) => {
     let buttonValue = event.currentTarget.value;
-    if (!isNaN(buttonValue)) {
+    if (!isNaN(buttonValue) || buttonValue === '.') {
       handleNumber(buttonValue);
-    } else if (buttonValue === '.') {
-      if (
-        !currentValue
-          .toString()
-          .split(previousValue + operator)
-          .join('')
-          .includes('.') ||
-        !currentValue.toString().includes('.')
-      ) {
-        currentValue += '.';
+    } else if (buttonValue === 'clear') {
+      currentValue = '0';
+      firstValue = null;
+      secondValue = null;
+      operator = null;
+      lastOperation = null;
+      updateDisplay();
+    } else if (
+      buttonValue === 'negate' &&
+      currentValue !== '0' &&
+      currentValue !== 'Divide by zero'
+    ) {
+      if (!operator && !secondValue) {
+        currentValue = -currentValue;
+      } else if (operator && secondValue) {
+        calculate(true);
+        currentValue = -currentValue;
       }
 
       updateDisplay();
-    } else if (buttonValue === 'clear') {
-      currentValue = '0';
-      previousValue = null;
-      operator = null;
-      updateDisplay();
-    } else if (buttonValue === 'negate' && currentValue !== '0') {
-      currentValue = -currentValue;
-      updateDisplay();
-    } else if (['/', '*', '-', '+'].includes(buttonValue)) {
-      if (!isNaN(currentValue.toString()[currentValue.toString().length - 1])) {
-        handleOperator(buttonValue);
-      }
-    } else if (buttonValue === 'equal' && currentValue && operator) {
-      calculate(previousValue, currentValue, operator);
-      previousValue = null;
+    } else if (
+      ['/', '*', '-', '+'].includes(buttonValue) &&
+      currentValue !== 'Divide by zero'
+    ) {
+      handleOperator(buttonValue);
+    } else if (
+      buttonValue === 'equal' &&
+      currentValue &&
+      operator &&
+      secondValue
+    ) {
+      calculate();
     }
-    console.log('currentValue', currentValue);
-    console.log('previousValue', previousValue);
-    console.log('operator', operator);
   });
 });
